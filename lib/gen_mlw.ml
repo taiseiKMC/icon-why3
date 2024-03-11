6b5c98e88b17187037be86ac91b3f59b5e5b1994
Sota Sato <sota.sato@dailambda.jp>
2024-03-12 02:01:58 +0900

    Add st.balance consistent with contract_balance

--- lib/gen_mlw.ml
+++ lib/gen_mlw.ml
@@ -126,6 +126,7 @@ module T = struct
           (* Hack: && => /\ *)
           Tbinnop (of_expr e1, Dterm.DTand, of_expr e2)
       | Eapply (e1, e2) -> Tapply (of_expr e1, of_expr e2)
+      | Eif (e1, e2, e3) -> Tif (of_expr e1, of_expr e2, of_expr e3)
       | _ ->
           Format.eprintf "T.of_expr: unsupported: %a@."
             (Mlw_printer.pp_expr ~attr:true).closed e;
@@ -203,13 +204,14 @@ module E = struct
 end
 
 module Step_constant = struct
-  let mk source sender self amount level now : expr =
+  let mk source sender self amount balance level now : expr =
     E.mk_record
       [
         (qualid [ "source" ], source);
         (qualid [ "sender" ], sender);
         (qualid [ "self" ], self);
         (qualid [ "amount" ], amount);
+        (qualid [ "balance" ], balance);
         (qualid [ "level" ], level);
         (qualid [ "now" ], now);
       ]
@@ -218,6 +220,7 @@ module Step_constant = struct
   let sender st : expr = eapp (qualid [ "sender" ]) [ st ]
   let self st : expr = eapp (qualid [ "self" ]) [ st ]
   let amount st : expr = eapp (qualid [ "amount" ]) [ st ]
+  let balance st : expr = eapp (qualid [ "balance" ]) [ st ]
   let level st : expr = eapp (qualid [ "level" ]) [ st ]
   let now st : expr = eapp (qualid [ "now" ]) [ st ]
 end
@@ -496,6 +499,13 @@ module Generator (D : Desc) = struct
               call_ctx_wf @@ E.var_of_binder ctx;
               call_step_wf @@ E.var_of_binder st;
               call_param_wf_of contract @@ E.var_of_binder gparam;
+              E.mk_bin
+                (Step_constant.balance @@ E.var_of_binder st)
+                "="
+                (E.mk_bin
+                   (balance_of contract (E.var_of_binder ctx))
+                   "+"
+                   (Step_constant.amount @@ E.var_of_binder st));
               call_pre_of contract (E.var_of_binder st) (E.var_of_binder gparam)
                 (E.var_of_binder ctx);
             ];
@@ -558,6 +568,14 @@ module Generator (D : Desc) = struct
                                   (source @@ E.var_of_binder st)
                                   (self @@ E.var_of_binder st)
                                   (E.mk_var dst) (E.mk_var amt)
+                                  (M.fold
+                                     (fun _ c e ->
+                                       E.mk_if
+                                         (is_contract_of c @@ E.mk_var dst)
+                                         (E.mk_bin (balance_of c ctx) "+"
+                                            (E.mk_var amt))
+                                         e)
+                                     contracts (econst 0))
                                   (level @@ E.var_of_binder st)
                                   (now @@ E.var_of_binder st))
                             in
@@ -648,6 +666,19 @@ module Generator (D : Desc) = struct
          in
          let+ p = E.mk_any gparam_pty in
          wrap_assume ~assumption:(wf st)
+         @@ wrap_assume
+              ~assumption:
+                (T.of_expr
+                @@ M.fold
+                     (fun _ c e ->
+                       E.mk_if
+                         (is_contract_of c @@ Step_constant.self st)
+                         (E.mk_bin (Step_constant.balance st) "="
+                            (E.mk_bin
+                               (balance_of c (E.var_of_binder ctx))
+                               "+" (Step_constant.amount st)))
+                         e)
+                     contracts (expr Etrue))
          @@ let+ ctx = dispatch_transfer (E.var_of_binder ctx) st p in
             call_unknown ctx
     in
